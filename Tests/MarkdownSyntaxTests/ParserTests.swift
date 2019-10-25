c2ca0e55cd51408c9725e273aeb0d774c053705b
Heberti Almeida <hebertialmeida@gmail.com>
2019-10-25 09:06:15 -0400

    Test line breaks parsing

--- Tests/MarkdownSyntaxTests/ParserTests.swift
+++ Tests/MarkdownSyntaxTests/ParserTests.swift
@@ -7,7 +7,11 @@ final class ParserTests: XCTestCase {
         // given
         let input = "Line 1\nLine 2\rLine 3\r\nLine 4"
 
+        // when
+        let tree = try Parser().parse(text: input)
+
         // then
         XCTAssertEqual(input.lineOffsets.count, 4)
+        XCTAssertEqual((tree.children.first as? Paragraph)?.children.count, 7)
     }
 }
