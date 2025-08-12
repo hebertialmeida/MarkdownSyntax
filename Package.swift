41b8280f206b4469eabf6feee09245b8411df0b8
Heberti Almeida <hebertialmeida@gmail.com>
2025-08-11 22:40:19 -0300

    Bump the minimum requirements for Swift Concurrency

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "MarkdownSyntax",
-    platforms: [.macOS(.v10_13), .iOS(.v12), .tvOS(.v12), .watchOS(.v4)],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     products: [
         .library(name: "MarkdownSyntax", targets: ["MarkdownSyntax"]),
     ],
